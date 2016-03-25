6ce1de4696ab2ea1309b0e462931488548464979
Glenpl <kubamolinski@gmail.com>
2016-03-25 14:11:43 +0100

    added get_signature to ChannelPageParser

--- youtube/channelpageparser.py
+++ youtube/channelpageparser.py
@@ -17,4 +17,23 @@ class ChannelPageParser(BaseParser):
 
     def get_signature(self, page_html):
         self._initialize_parser(repr(page_html))
-        # return ChannelSignature()
+        return ChannelSignature(self._extract_id(),
+                                self._extract_name(),
+                                self._extract_videos_amount(),
+                                self._extract_subscriptions(),
+                                self._extract_thumbnail_url())
+
+    def _extract_id(self):
+        return self._html_parser.select('.qualified-channel-title-text a')[0]['href'].split('/')[2]
+
+    def _extract_name(self):
+        return str(self._html_parser.select('.qualified-channel-title-text a')[0].string)
+
+    def _extract_videos_amount(self):
+        return 'NOTSET'  # data can't be extracted
+
+    def _extract_thumbnail_url(self):
+        return self._html_parser.select('.channel-header-profile-image')[0]['src']
+
+    def _extract_subscriptions(self):
+        return self._remove_non_breaking_spaces(str(self._html_parser.select('.about-stats span b')[0].string))
