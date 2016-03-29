14e572d5a1d253e6d3b32dd182ed7f289feb27db
Glenpl <kubamolinski@gmail.com>
2016-03-29 12:16:13 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -5,26 +5,20 @@ from os import path
 
 HERE = path.abspath(path.dirname(__file__))
 
-with open_(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
-
 with open('requirements.txt') as f:
     REQUIRED = f.read().splitlines()
 
 setup(
     name='open-yt-api',
-    version='0.0.1',
-    description='open YouTube API library',
-    long_description=LONG_DESCRIPTION,
+    version='1.0.0',
+    description='Open YouTube API library',
     url='https://github.com/Glenpl/open-yt-api',
+    download_url = 'https://github.com/Glenpl/open-yt-api/tarball/1.0.0'
     author='Glenpl/Jakub Molinski',
     author_email='kubamolinski@gmail.com',
     license='MIT',
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 1',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
